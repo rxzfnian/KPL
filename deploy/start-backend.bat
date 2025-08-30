e4f7f0d8df98bf2ef8d90bf8c16e51d643277247
rxzfnian <woyaodeziy@outlook.com>
2025-08-30 12:37:13 +0800

    Fix backend deployment issues: clean dependencies, fix Koyeb config, optimize startup script

--- deploy/start-backend.bat
+++ deploy/start-backend.bat
@@ -1,4 +1,12 @@
 @echo off
-echo Starting backend server...
+echo Starting KPL Guessr Backend Server...
+echo Current directory: %CD%
+echo Node version: 
+node --version
+echo NPM version:
+npm --version
+echo Installing dependencies...
+npm install
+echo Starting server...
 npm start
 pause
